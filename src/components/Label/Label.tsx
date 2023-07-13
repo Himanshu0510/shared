57da3350362e47de802e363ec62785785b7dd462
Himanshu Chauhan <consultant.frontend1@adoptnet.tech>
2023-07-13 16:23:52 +0530

    code updated

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Label = () => (
+const Label = (props: any) => (
   <>
-    Welcome <input type="text" />{" "}
+    Welcome {props.name} <input type="text" />{" "}
   </>
 );
 
